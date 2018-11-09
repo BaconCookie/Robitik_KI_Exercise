c0d96ecfa82b8cf0ee0e899aa524bf4e5248377e
BaconCookie <laura.hartgers@posteo.net>
2018-11-09 16:41:01 +0100

    Add my solution for Exercise 1 in Prediction.py

--- excercise_1/catkin_ws/src/prediction/src/Prediction.py
+++ excercise_1/catkin_ws/src/prediction/src/Prediction.py
@@ -16,33 +16,65 @@ class Prediction:
 
         self.sensor_msgs.msg = CompressedImage()
 
+        # SUBSCRIBE
+        # subscribe to receive image data from /camera/output/specific/compressed_img_msgs
+        rospy.Subscriber('camera/output/specific/compressed_img_msgs',
+                         CompressedImage,
+                         self.handle_received_img)
+
+        #Subscribe to /camera/output/specific/check to verify own prediction
+        rospy.Subscriber('/camera/output/specific/check',
+                         Bool,
+                         self.verify_own_prediction)
+
+        # PUBLISH to '/camera/input/specific/number'
+        self.publish_predicted = rospy.Publisher('/camera/input/specific/number',
+                                                 Int32,
+                                                 queue_size=1)
+
         # load keras model, filepath = "models/weights-best.hdf5"
-        model = tf.keras.models.load_model(
+        self.model = tf.keras.models.load_model(
             "models/weights-best.hdf5",
             custom_objects=None,
             compile=True
         )
 
+        # manage TensorFlow threads
+        self.session = k.get_session()
+        self.graph = tf.get_default_graph()
+        self.graph.finalize()
+
+
+    def handle_received_img(self, img):
+        # convert img
+        image = self.cv_bridge.compressed_imgmsg_to_cv2(img)
+
+        # use keras model to predict the content (number 0-9) on the image
+        prediction = self.model.predict(image)
+        # revert from one-hot encoding
+        prediction_as_real_number = np.argmax(prediction, axis=None, out=None)
+
+        # publish result of prediction to /camera/input/specific/number
+        self.publish_predicted.publish(prediction_as_real_number)
+
+
+    def verify_own_prediction(self, bool):
+        print('prediction was: ', bool)
 
 def main():
     try:
         # register node
         rospy.init_node('prediction', anonymous=False)
 
-        # init CameraPseudo
+        # init Prediction
         pred = Prediction()
 
+        # spin() simply keeps python from exiting until this node is stopped
         rospy.spin()
 
-        # TODO while loop
-        while not rospy.is_shutdown():
-            print('rospy lives, while loop in Prediction')
+        #  maybe needed??? while loop?  while not rospy.is_shutdown():
 
-            # TODO publish/subscribe zeug, (gegenstück zu komprimierung nutzen um das bild lesen zu können)
-
-            # TODO benutze keras modell zum predicten
-
-            # TODO threads managen! sehe link in readme
+        # TODO publish/subscribe zeug, (gegenstueck zu komprimierung nutzen um das bild lesen zu koennen)
 
     except rospy.ROSInterruptException:
         pass
